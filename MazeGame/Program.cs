86b265d13b06df4d702a9c6cfd36ffcb67407cc6
RanKohde <rankohde@gmail.com>
2018-11-13 21:52:05 +0900

    大まかな処理の流れをメモ

--- MazeGame/Program.cs
+++ MazeGame/Program.cs
@@ -10,7 +10,13 @@ namespace MazeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // 迷路生成オプションを受け取る
+
+            // 迷路が固定サイズか可変サイズかによって使用する迷路生成メソッドを分ける
+
+            // ゲーム開始準備
+
+            
         }
     }
 }
