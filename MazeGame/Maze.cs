29c2b0edf9dafcc2808278c1daf96bc2d3a049ce
RanKohde <rankohde@gmail.com>
2018-11-15 20:56:35 +0900

    プロパティに説明を追加

--- MazeGame/Maze.cs
+++ MazeGame/Maze.cs
@@ -2,12 +2,37 @@
 {
     public class Maze
     {
+        /// <summary>
+        /// 保持している迷路の横幅
+        /// </summary>
         public int Width { get; private set; }
+
+        /// <summary>
+        /// 保持している迷路の縦幅
+        /// </summary>
         public int Height { get; private set; }
+
+        /// <summary>
+        /// 保持している迷路配列
+        /// </summary>
         public char[,] Map { get; private set; }
+
+        /// <summary>
+        /// 迷路のスタート地点
+        /// </summary>
         public int[] Start { get; private set; }
+
+        /// <summary>
+        /// 迷路のゴール地点
+        /// </summary>
         public int[] Goal { get; private set; }
 
+        /// <summary>
+        /// 新しい迷路インスタンスを生成します
+        /// </summary>
+        /// <param name="maze">迷路を表す2次元配列</param>
+        /// <param name="start">迷路のスタート地点</param>
+        /// <param name="goal">迷路のゴール地点</param>
         public Maze(char[,] maze, int[] start, int[] goal)
         {
             Map = maze;
