ea1f19ea5a5508bdccf911e20b874f8643165625
rankohde <rankohde@gmail.com>
2018-11-15 15:32:41 +0900

    （仮）フィールドやプロパティを定義

--- MazeGame/Maze.cs
+++ MazeGame/Maze.cs
@@ -1,1 +1,20 @@
-﻿public class Maze { }
+﻿namespace MazeGame
+{
+    public class Maze
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public char[,] Map { get; private set; }
+        public int[] Start { get; private set; }
+        public int[] Goal { get; private set; }
+
+        public Maze(char[,] maze, int[] start, int[] goal)
+        {
+            Map = maze;
+            Start = start;
+            Goal = goal;
+            Width = maze.GetLength(0);
+            Height = maze.GetLength(1);
+        }
+    }
+}
